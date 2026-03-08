ce6ed35aa1ca6900e4ee7ad996803cf6cbf08ac9
Dagim Sime <simedagim121@gmail.com>
2026-03-09 00:07:13 +0300

    feat(js/easy): add text to speech converter

---
+++ javascript/easy/text-to-speech/script.js
@@ -0,0 +1,46 @@
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+const stopButton = document.getElementById("stop-button");
+const textInput = document.getElementById("text");
+const speedInput = document.getElementById("speed");
+let currentCharacter;
+
+playButton.addEventListener("click", () => {
+  playText(textInput.value);
+});
+
+pauseButton.addEventListener("click", pauseText);
+stopButton.addEventListener("click", stopText);
+speedInput.addEventListener("input", () => {
+  stopText();
+  playText(utterence.text.substring(currentCharacter));
+});
+
+const utterence = new SpeechSynthesisUtterance();
+utterence.addEventListener("end", () => {
+  textInput.disabled = false;
+});
+utterence.addEventListener("boundary", (e) => {
+  currentCharacter = e.charIndex;
+});
+
+function playText(text) {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
+  if (speechSynthesis.speaking) return;
+  utterence.text = text;
+  utterence.rate = speedInput.value || 1;
+
+  textInput.disabled = true;
+  speechSynthesis.speak(utterence);
+}
+
+function pauseText() {
+  if (speechSynthesis.speaking) speechSynthesis.pause();
+}
+
+function stopText() {
+  speechSynthesis.resume();
+  speechSynthesis.cancel();
+}
